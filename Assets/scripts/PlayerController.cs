e222bffe1554208bb006feaf0da9734b1ba9a419
Mico Espina <Mico Espina>
2017-03-12 22:42:50 +0800

    first commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 0.0f;
+    public Vector2 velocityCheck = new Vector2(20.0f, 0.0f);
+    private Animator animator;
+    public PlayerInput input;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+        animator = GetComponent<Animator>();
+        input = GetComponent<PlayerInput>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float forceX = 0.0f;
+        float forceY = 0.0f;
+
+            var playerAbsVelX = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x);
+      //  var playerAbsVelY = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y);
+
+        if (playerAbsVelX <= 1.0f /*&& playerAbsVelY <= 0.1f*/)
+        {
+            animator.SetInteger("animState", 0);
+        }
+
+        if (input.moving.x != 0)
+        {
+            forceX = moveSpeed * input.moving.x;
+            if (playerAbsVelX <= velocityCheck.x)
+            {                
+                Debug.Log("player" + playerAbsVelX+ "limit" + velocityCheck.x);
+            }
+            if (input.moving.x>0)
+            {
+                animator.SetInteger("animState", 1);
+            }
+            if (input.moving.x < 0)
+            {                
+                animator.SetInteger("animState", -1);
+            }
+        }
+
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(forceX, forceY), ForceMode2D.Impulse);
+
+    }
+}
---
+++ Assets/scripts/PlayerInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    public Vector2 moving = new Vector2();
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        moving.x = 0;
+        moving.y = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moving.x = 1;
+           
+            Debug.Log("moving.x"+moving.x);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moving.x = -1;
+            Debug.Log("moving.x"+moving.x);
+        }else
+        {
+        }
+
+	}
+}
